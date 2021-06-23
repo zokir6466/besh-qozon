17ef3bfa735e3ab3c918ab33067eea705bc4ffb4
zokir <you@example.com>
2021-06-23 14:15:55 +0500

    zokir

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '146ce01fc5431aeea079074829d46186'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0c4ef70c-5ec5-4778-8e55-ca9409b75683'
+};
